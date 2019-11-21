ef1e52d67ea48460d5f9f30371592d28b5653271
Serghei Gurgurov <serghei.gurgurov@gmail.com>
2019-11-21 15:21:21 +0100

    removed template handlebars route

--- routeMapper.js
+++ routeMapper.js
@@ -53,28 +53,6 @@ const routingMap = {
             body: templateManager.t_characters
         }
     },
-    '/handlebarsProject': {
-        data: {
-            head: function () {
-                return {
-                    title: "Handlebars-proj",
-                    styles: ["bootstrap.min.css", "handlebars-style.css"],
-                    scripts: []
-                }
-            },
-            body: function () {
-                return {
-                    blocks: []
-                }
-            }
-        },
-        template: {
-            head: templateManager.defaultHeadTemplate,
-            body: function (body) {
-                return `<div class="container-fluid" id="pagina"> <div class="row" style="height:undefined;background-color: #B3E85C;"> <div class="header" style="font-size:60px;"> header </div></div><br><br><div class="row" style="font-size:30px;margin-left:30px;"> Questo e' un titolo </div><div class="row"> <div class="col-md-11 paragraph"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </div></div><div class="row"> <div class="col-md-12" style="padding:40px 0px;"> <div class="row" style="margin: 0px 30px;"> <div class="col-md-3 card"> <img src=https://a0.muscache.com/im/pictures/998b4270-25d7-4850-8ccf-4350b9a5ab05.jpg> <div class="cardText"> Parigi </div></div><div class="col-md-3 card"> <img src=https://a0.muscache.com/im/pictures/60145c65-7c36-4ac5-8129-6ae9a0d27a81.jpg> <div class="cardText"> Tokyo </div></div><div class="col-md-3 card"> <img src=https://a0.muscache.com/im/pictures/6729455e-af21-4dc3-bfdf-332393d407a8.jpg> <div class="cardText"> Londra </div></div><div class="col-md-3 card"> <img src=https://a0.muscache.com/im/pictures/82af5bc4-ed5b-44d9-9209-ac0f2fbf8986.jpg> <div class="cardText"> Los Angeles </div></div></div></div></div><div class="row" style="font-size:30px;margin-left:30px;"> Questo e' un'altro titolo </div><div class="row"> <div class="col-md-5 paragraph"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </div><div class="col-md-6"> <img src=img1.png> </div></div><br><br><div class="row" style="background-color:#B3E85C;height:70px;"> </div></div>`
-            }
-        }
-    },
     '/login': {
         data: {
             head: function () {
@@ -160,6 +138,5 @@ const routingMap = {
         }
     }
 }
-
 
 exports.routingMap = routingMap;
