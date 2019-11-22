ab0cc0a713e12182700aa5996480b5384e575503
Serghei Gurgurov <serghei.gurgurov@gmail.com>
2019-11-22 14:52:10 +0100

    Helllo World Test

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ const dbClient = require("./dbManager").dbClient;
 
 const port = process.env.PORT || 5000;
 
-dbClient.init(function () {
+//dbClient.init(function () {
     http.createServer(async function (req, res) {
         //util.log(`ServerRequest`, `${req.url}  index.js:${__line}`, util.colors.FgGreen);
-        pageLoader.showPage(res, req);
+
+        res.writeHead(200);
+        res.write("Hello World");
+        res.end();
+
+        //pageLoader.showPage(res, req);
     }).listen(port);
-});
+//});
 
 util.log('Server Log', `server started o port: ${port}  index.js:${__line} `, util.colors.FgCyan);
 
